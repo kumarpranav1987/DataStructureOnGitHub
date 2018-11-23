7d1846b32c34672d7778e7c22d68022ddc1a75ab
pranav.kumar@iongroup.com <pranav.kumar@iongroup.com>
2018-11-23 18:25:24 +0530

    https://www.hackerearth.com/practice/algorithms/graphs/shortest-path-algorithms/practice-problems/algorithm/routes-48c6192a/

---
+++ src/main/java/com/pranav/hackerearth/Routes.java
@@ -0,0 +1,17 @@
+package com.pranav.hackerearth;
+
+import java.util.Scanner;
+
+//https://www.hackerearth.com/practice/algorithms/graphs/shortest-path-algorithms/practice-problems/algorithm/routes-48c6192a/
+public class Routes {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		double earningPerHour = s.nextDouble();
+		int numberOfCitites = s.nextInt();
+		int numberOfRoutes = s.nextInt();
+		
+
+	}
+
+}
